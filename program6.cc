b005933f14e38fd64fa905f28f46ea63b990f292
Daniel Casper <dc@danielcasper.com>
2018-04-19 20:42:02 -0500

    totally borked windows

--- program6.cc
+++ program6.cc
@@ -5,22 +5,31 @@
 #include "cdk.h"
 #include <sys/stat.h>
 #include <iostream>
-
-#define MATRIX_WIDTH 3
-#define MATRIX_HEIGHT 3
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <unistd.h>
+#define MATRIX_WIDTH 2
+#define MATRIX_HEIGHT 4
 #define BOX_WIDTH 15
 #define MATRIX_NAME_STRING "Text Matrix"
 
 using namespace std;
 
 int main(int argc, char * argv[]) {
+	struct BinaryFileHeader {
+		uint32_t magicNumber;
+		uint32_t versionNumber;
+		uint64_t numRecords;
+	} headers;
+
 	WINDOW *window;
 	CDKSCREEN *cdkscreen;
 	CDKMATRIX *myMatrix;
-	const char *rowTitles[MATRIX_HEIGHT+1] = {"R0", "R1", "R2", "R3" };
-	const char *columnTitles[MATRIX_WIDTH+1] = {"C0", "C1", "C2", "C3" };
-	int boxWidths[MATRIX_WIDTH+1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
-	int boxTypes[MATRIX_WIDTH+1] = {vMIXED, vMIXED, vMIXED, vMIXED};
+	const char *rowTitles[MATRIX_HEIGHT+1] = { "a", "b", "c", "d", "e" };
+	const char *columnTitles[MATRIX_WIDTH+1] = { "a", "b", "c" };
+	int boxWidths[MATRIX_WIDTH+1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
+	int boxTypes[MATRIX_WIDTH+1] = {vMIXED, vMIXED, vMIXED};
 
 	window = initscr();
 	cdkscreen = initCDKScreen(window);
@@ -33,19 +42,45 @@ int main(int argc, char * argv[]) {
 		printf("Error in creating matrix");
 		_exit(1);
 	}
+
+	FILE *file;
+	file = fopen("cs3377.bin", "rb");
+
+//hile(1) {
 	
-	struct stat results;
+	fread(&headers,sizeof(headers),1,file);
 
-	if(stat("cs3377.bin", &results) == 0) {
-		cout << results.st_size;
-	}
-	else {
-		return 1;
-	}
+	fclose(file);
 
+
+//if(feof(file) != 0)
+//			break;
+//		printf
+//	}
+//	fstream file;
+//	file.open("cs337.bin", fstream::in | fstream::binary);
+//	file.read((char*)&headers.magicNumber, sizeof(headers.magicNumber));
+//	file.read((char*)&headers.versionNumber, sizeof(headers.versionNumber));
+//	file.read((char*)&headers.numRecords, sizeof(headers.numRecords));
+//	file.read(&headers, sizeof(headers));
+//	file.close();
+
+//	struct stat results;
+//	cout << headers.magicNumber;
+//cout << headers.versionNumber << "\n";
+//cout << headers.numRecords;
+//
+//	if(stat("cs3377.bin", &results) == 0) {
+//		cout << results.st_size;
+//	}
+//	else {
+//		return 1;
+//	}
+	string versionNumber = "Version: " + to_string(headers.versionNumber);
+	string numRecords = "NumRecords: " + to_string(headers.numRecords);
 	drawCDKMatrix(myMatrix, true);
-	setCDKMatrixCell(myMatrix, 2, 2, "message");
-	drawCDKMatrix(myMatrix, true);
+	setCDKMatrixCell(myMatrix, 1, 2, versionNumber.c_str());
+	setCDKMatrixCell(myMatrix, 1, 3, numRecords.c_str());
 
 	sleep(10);
 
